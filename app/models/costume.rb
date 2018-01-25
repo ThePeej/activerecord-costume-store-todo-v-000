eb0c5259c8c1c1d87dd16896860fa7e6d817fd1a
PJ Frias <peejfrias@gmail.com>
2018-01-25 21:06:00 +0000

    automatically backed up by learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,10 +1,2 @@
 class Costume < ActiveRecord::Base[5.0]
-  def change
-    create_table :costumes do |c|
-      c.string :name
-      c.integer :price
-      c.integer :size
-      c.text :image_url
-    end
-  end
 end
